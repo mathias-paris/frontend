c4bbbef4615cbe4f78b64adbc866b8b25ad76e30
Mathias Paris <mathias@integon.ch>
2024-04-25 08:35:36 +0200

    fix filename login

--- public/config.js
+++
@@ -1,4 +0,0 @@
-window.config = {
-    tokenUrl:'https://a854b3f2-20d4-4081-be24-f561328394d8-dev-internal.e1-us-east-azure.internal.choreoapis.dev/ybxk/tokenservice/auth-22e/v1.0/generateToken',
-    backendUrl:'https://a854b3f2-20d4-4081-be24-f561328394d8-dev.e1-us-east-azure.choreoapis.dev/ybxk/time-sheet-backend-service/timesheet-61d/v1/timeSheet'
-}
